a67307db42656659da5acae6d25b3e70b57b483b
Dimi <stefangeorges4799@gmail.com>
2023-08-03 13:14:09 -0300

    Finish setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Grid, Typography} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography>
+            MessageForm
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>
+            Messages
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
