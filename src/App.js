d26ad8fdd1ce82cc544477def817996b081af22e
ccatalini <camila.catalini@ing.austral.edu.ar>
2023-08-14 09:45:18 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
+
 import './App.css';
-import { Container, Grid, Box } from '@mui/material';
-import NavBar from './NavBar';
-import MessageForm from './MessageForm';
-import Messages from './Messages';
+
 
 function App() {
-    const [messageList, setMessageList] = useState([]);
-
-    const handleMessageSubmit = (message) => {
-        setMessageList([...messageList, message]);
-    };
-
     return (
-        <Container className="App">
-            <NavBar />
-            <Box mt={2}> {/* Agregar margen superior de 2 unidades */}
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <MessageForm onMessageSubmit={handleMessageSubmit} />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Messages messages={messageList} />
-                    </Grid>
-                </Grid>
-            </Box>
-        </Container>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
 }
 
