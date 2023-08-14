0f1e159a8236418483514a1cab2439204ec1c04b
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2023-08-13 23:15:47 -0300

    challenge-Lucas-Lovaglio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
+import React, { useState } from 'react';
 import './App.css';
-import {Container, Grid, Typography} from "@mui/material";
+import {
+  Container,
+  Grid,
+  Typography,
+  AppBar,
+  Toolbar,
+  TextField,
+  Box,
+  Button,
+  FormGroup,
+  FormControl,
+  FormLabel,
+} from '@mui/material';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const addMessage = () => {
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, newMessage]);
+      setNewMessage('');
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
   return (
-    <Container className="App">
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography>
-            MessageForm
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+            Message App
           </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className='App'>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h6">Message Form</Typography>
+            <FormGroup controlId="name">
+              <FormControl>
+                <TextField
+                  type="text"
+                  placeholder="Enter message"
+                  value={newMessage}
+                  onChange={handleInputChange}
+                />
+              </FormControl>
+            </FormGroup>
+            <Button onClick={addMessage}>Sent</Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h6">Messages</Typography>
+            {messages.length !== 0 ? (
+            <ul>
+              {messages.map((message, index) => (
+                <li key={index}>{message}</li>
+              ))}
+            </ul>
+            ) : (
+              <Typography variant="p" sx={{color: 'gray'}}>There aren't any message</Typography>
+            )}
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Typography>
-            Messages
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Box>
   );
 }
 
