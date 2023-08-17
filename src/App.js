30594833b4428d8bae4f418fce56a09d67c70d09
rjosue6 <josue.riera@ing.austral.edu.ar>
2023-08-17 14:08:02 -0300

    navbar prueba

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Container, Grid, Box } from '@mui/material';
-import NavBar from './NavBar';
-import MessageForm from './MessageForm';
-import Messages from './Messages';
+import NavBar from './components/NavBar';
+
 
 function App() {
-    const [messageList, setMessageList] = useState([]);
-
-    const handleMessageSubmit = (message) => {
-        setMessageList([...messageList, message]);
-    };
 
     return (
-        <Container className="App">
-            <NavBar />
-            <Box mt={2}> {/* Agregar margen superior de 2 unidades */}
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <MessageForm onMessageSubmit={handleMessageSubmit} />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Messages messages={messageList} />
-                    </Grid>
-                </Grid>
-            </Box>
-        </Container>
+        <div className="App">
+            <NavBar/> 
+        </div>
     );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import "./NavBarStyles.css"
+
+function NavBar() {
+    return (
+       <nav>
+        <div>
+            <ul id="navbar">
+                <li> Message App </li>
+            </ul>
+        </div>
+       </nav>
+    );
+};
+
+export default NavBar;
